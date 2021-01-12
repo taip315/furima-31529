cacb1dd4b4ffcca9e2963f3bf6134ea686725cb9
taip315 <beaeaeat@gmail.com>
2021-01-12 10:10:48 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,14 @@ class Item < ApplicationRecord
     belongs_to :prefecture
     belongs_to :day
 
-    with_options numericality: {other_than: 1} do 
+    with_options presence:true do
+      validates :image
+      validates :name
+      validates :explanation
+      validates :price, numericality: {greater_than: 300, less_than: 9999999}, format: {with:/\d/}
+    end 
+
+    with_options numericality: {other_than: 1}, presence: true do 
       validates :category_id
       validates :state_id
       validates :delivery_fee_id
