fb0a830f556cd15203067a41e8c8ac10c1c68550
taip315 <beaeaeat@gmail.com>
2021-01-19 23:29:25 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
+  before_action :authenticate_user! , except: [:index, :show]
   before_action :get_item, only: [:show,:edit,:update,:destroy]
   before_action :ensure_correct_user, only:[:edit, :update, :destroy]
 
